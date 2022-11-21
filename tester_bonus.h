2f149bba65bde51bcb1e92a3630aabd0f7b4da7c
Deesse75 <dsfone75@gmail.com>
2022-11-21 14:36:03 +0100

    Update tester_bonus.h

--- tester_bonus.h
+++ tester_bonus.h
@@ -5,7 +5,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <unistd.h>
-# include "../get_next_line/get_next_line.h"
+# include "../get_next_line.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 8
